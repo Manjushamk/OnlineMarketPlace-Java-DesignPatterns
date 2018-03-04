8811b69d48c9094cba9d448293b0d10cc972ea3d
mkottala <mkottala@iupui.edu>
2018-03-04 17:49:47 -0500

    Invocation Handler Class creation

---
+++ AuthorizationInvocationHandler.java
@@ -0,0 +1,34 @@
+// Honor Pledge:
+//
+// I pledge that I have neither given nor 
+// received any help on this assignment.
+//
+//mkottala
+
+import java.io.Serializable;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class AuthorizationInvocationHandler implements InvocationHandler, Serializable {
+	private Object objectImpl;
+
+	public AuthorizationInvocationHandler(Object impl) {
+		this.objectImpl = impl;
+	}
+
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		if (method.isAnnotationPresent(RoleAnnotation.class)) {
+			RoleAnnotation test = method.getAnnotation(RoleAnnotation.class);
+			Session session = (Session) args[0];
+
+			if (session.getUser().getRoleType().equals(test.value())) {
+				return method.invoke(objectImpl, args);
+			} else {
+				throw new AuthorizationException(method.getName());
+			}
+		} else {
+			return method.invoke(objectImpl, args);
+		}   
+	}
+}
