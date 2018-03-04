16830859d8767db2abed179d20e73475881ffdf9
mkottala <mkottala@iupui.edu>
2018-03-04 17:40:14 -0500

    implementing Annotation functionality in MarketPlace interface

--- MarketPlace.java
+++ MarketPlace.java
@@ -15,9 +15,13 @@ import java.rmi.Remote;
 
 public interface MarketPlace extends Remote {
 	//interfaces that MarketPlaceView uses remotely
-	 boolean userLogin(Session session, String userId, String password, String type) throws java.rmi.RemoteException;
-	 boolean adminLogin(Session session, String userId, String password, String type) throws java.rmi.RemoteException;
-	 Session sessionLogin(String request) throws java.rmi.RemoteException;
-	 String register(String userName, String userId, String password) throws java.rmi.RemoteException;
-	 String[] getItems(String itemType) throws java.rmi.RemoteException;
+	@RoleAnnotations("User")
+	boolean userLogin(Session session, String userId, String password, String type) throws java.rmi.RemoteException;
+
+	@RoleAnnotations("Admin")
+	boolean adminLogin(Session session, String userId, String password, String type) throws java.rmi.RemoteException;
+
+	Session sessionLogin(String request) throws java.rmi.RemoteException;
+	String register(String userName, String userId, String password) throws java.rmi.RemoteException;
+	String[] getItems(String itemType) throws java.rmi.RemoteException;
 }
