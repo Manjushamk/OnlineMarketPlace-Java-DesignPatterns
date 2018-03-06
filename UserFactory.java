c3342e6e5362bc4a548c80496d25969c655cd953
Kottala, Manjusha <mkottala@iupui.edu>
2018-03-06 18:53:50 -0500

    updated comments

--- UserFactory.java
+++ UserFactory.java
@@ -7,8 +7,8 @@
 
 // Factory class implementation for User extending abstract factory class
 public class UserFactory extends AbstractFactory{
+	
 	//Overriding the methods for selection of the concrete class
-
 	@Override
 	public User getUserView(String type) {
 		if(type.equalsIgnoreCase("user")) {
@@ -16,7 +16,7 @@ public class UserFactory extends AbstractFactory{
 		}
 		return null;
 	}
-
+	//overriding admin view but instance shouldn't be created
 	@Override
 	Admin getAdminView(String type) {
 		return null;
