0f84e17c16fd4c351e96c53e2a648528be3c71e8
Kottala, Manjusha <mkottala@iupui.edu>
2018-03-06 18:57:49 -0500

    updated comments

--- RoleAnnotations.java
+++ RoleAnnotations.java
@@ -10,8 +10,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+//user defined annotation which acts on METHOD and TYPE at run time
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RoleAnnotations {
+	//value() method returns a stored annotaion value in String format
 	String value();
 }
