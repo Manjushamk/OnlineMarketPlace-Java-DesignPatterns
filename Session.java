c64ef4732695ae26b17409207841cc8ace5d9ae6
Kottala, Manjusha <mkottala@iupui.edu>
2018-03-06 18:56:26 -0500

    updated comments

--- Session.java
+++ Session.java
@@ -8,14 +8,18 @@
 
 import java.io.Serializable;
 
+//Session class implements from Serializable and this is created based on the user role
+//A new session object is created and passed over the application
 public class Session implements Serializable{
 
 	String roleType;
-
+	
+	//constrcutor to intialize roleType
 	public Session(String roleType){
 		this.roleType=roleType;
 	}
 
+	//return new role type
 	public String getRoleType(){
 		return roleType;
 	}
