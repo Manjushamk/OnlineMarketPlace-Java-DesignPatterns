a93e62754f90697d3ee8ff14830c01c0b16db211
Kottala, Manjusha <mkottala@iupui.edu>
2018-03-06 18:47:31 -0500

    updated comments

--- AuthorizationInvocationHandler.java
+++ AuthorizationInvocationHandler.java
@@ -5,23 +5,30 @@
 //
 //mkottala
 
+//importing libraries for reflection and object passing
 import java.io.Serializable;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
+//AuthorizationInvocationHandler implements from Invocation Handler and Serializable to over ride
+//invoke method and code with appropriate functionality
 public class AuthorizationInvocationHandler implements InvocationHandler, Serializable {
 	private Object objectImpl;
-
+	
+	//constructor
 	public AuthorizationInvocationHandler(Object impl) {
 		this.objectImpl = impl;
 	}
-
+	
+	//overiding invoke method which can invoke an appropriate method
+	//if the annotation from interface and session both matches
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		//metadata checking using java reflections
 		if (method.isAnnotationPresent(RoleAnnotations.class)) {
 			RoleAnnotations test = method.getAnnotation(RoleAnnotations.class);
 			Session session = (Session) args[0];
-
+			
 			if (session.getRoleType().equals(test.value())) {
 				return method.invoke(objectImpl, args);
 			} else {
