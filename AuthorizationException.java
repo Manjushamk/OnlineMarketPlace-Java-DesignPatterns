5918c9b8e6a22da303547e581fc304a4a6d2fe6a
Kottala, Manjusha <mkottala@iupui.edu>
2018-03-06 18:43:51 -0500

    updated comments

--- AuthorizationException.java
+++ AuthorizationException.java
@@ -7,6 +7,7 @@
 
 //user defined exception class for Authorization
 public class AuthorizationException extends RuntimeException{
+	//this is called when an error or exception occurs
 	public AuthorizationException(String functionName) {
 		super("Access Denined to " + functionName);
 	}
