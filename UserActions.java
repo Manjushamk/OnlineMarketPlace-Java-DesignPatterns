fc5a593fba885d1e21885031be1484013fb17333
mkottala <mkottala@iupui.edu>
2018-04-02 22:34:09 -0400

    Adding interface for user commands

---
+++ UserActions.java
@@ -0,0 +1,11 @@
+// Honor Pledge:
+//
+// I pledge that I have neither given nor 
+// received any help on this assignment.
+//
+//mkottala
+
+//command interface for user commands
+public interface UserActions {
+	void execute();
+}
